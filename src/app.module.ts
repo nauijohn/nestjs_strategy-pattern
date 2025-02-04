52c1f7fb2288f494e17fdfe6bf5dc783dcb837e1
alctech-nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-02-04 18:49:02 +0800

    removed unnecessary files/folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PaymentsModule } from './payments/payments.module';
 
 @Module({
   imports: [PaymentsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
